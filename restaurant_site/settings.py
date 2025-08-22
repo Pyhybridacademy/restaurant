2c30813b00001429fa883f1f271b955cb134a4aa
edike philomena <edikephilomena@gmail.com>
2025-08-22 14:36:39 +0100

    second

--- restaurant_site/settings.py
+++ restaurant_site/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-7f=^@7#^7lp-#*4va=77j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'restaurant-ra9l.onrender.com', '.vercel.app']
 
 # Application definition
 INSTALLED_APPS = [
